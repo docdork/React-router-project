018fe84eab091f818671e9fffca90ff1b94d3bf0
David_Holliday <dhholliday@icloud.com>
2023-04-24 19:27:48 +0100

    Defining and using dynamic routes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import HomePage from './pages/Home';
 import ProductsPage from "./pages/Products";
 import RootLayout from './pages/Root';
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
-    errorElement: <ErrorPage/>,
+    errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage /> }
     ]
   }]
 );
